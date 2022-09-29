46868c96872537c9f1b65dc5365f72fa5eb12dea
Baris34 <46277882+Baris34@users.noreply.github.com>
2022-09-29 12:20:27 +0300

    variables

---
+++ variables/src/variables/variables.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class variables {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 51;
+		String mesaj ="Öğrenci sayısı : ";
+          System.out.println(mesaj + ogrenciSayisi);
+          System.out.println("Öğrenci sayım : 10");
+          System.out.println("Öğrenci sayım : 10");
+          System.out.println("Öğrenci sayım : 10");
+	}
+
+}
