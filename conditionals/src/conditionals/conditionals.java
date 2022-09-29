010cae903bfbe493ef01a5a054724e2cdbe19e32
Baris34 <46277882+Baris34@users.noreply.github.com>
2022-09-29 12:52:01 +0300

    conditionals

---
+++ conditionals/src/conditionals/conditionals.java
@@ -0,0 +1,24 @@
+package conditionals;
+
+public class conditionals {
+
+	public static void main(String[] args) {
+		int sayi = 30;
+		if (sayi<20){
+			System.out.println("sayı 20den küçüktür.");}
+		
+		else if (sayi==20) {
+	   System.out.println("Sayı 20ye eşittir.");		
+		}
+		
+		else {
+		System.out.println("sayı 20den büyüktür.");	
+		}
+		
+		
+		
+		
+		
+		
+     }
+}
